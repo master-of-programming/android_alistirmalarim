20ca263ea59acf5aef74d1b54cbd8c08442c70a0
master-of-programming <therealprogrammerboy@protonmail.com>
2020-06-28 13:36:52 +0300

    butona özellik eklendi

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_click_me = findViewById<Button>(R.id.button) as Button
+        val text = findViewById<TextView>(R.id.text) as TextView
+        btn_click_me.setOnClickListener {
+            text.setText("Tıklandı.")
+            Toast.makeText(applicationContext, "Tıklandı.", Toast.LENGTH_LONG).show()
+        }
     }
 }
