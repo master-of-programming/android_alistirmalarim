07bed8588887bd515450f3cac3c9a67af11fad0f
master-of-programming <therealprogrammerboy@protonmail.com>
2020-06-28 23:09:55 +0300

    java ile butonlar

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tanimla();
+        islevVer();
+    }
+    private void tanimla(){
+        btn=findViewById(R.id.bir);
+    }
+    private void islevVer(){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            System.out.println("Butona Tıklayıp Durma Bozucan Salak !");
+            }
+        });
+    }
+}
